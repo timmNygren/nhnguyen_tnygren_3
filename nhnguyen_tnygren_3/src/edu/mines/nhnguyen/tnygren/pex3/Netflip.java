cefd3422e9fb72856957626ed8d611aa6185056b
Timm Nygren <metroid3prime@yahoo.com>
2014-03-31 18:59:27 -0600

    displays hello world in applet

--- nhnguyen_tnygren_3/src/edu/mines/nhnguyen/tnygren/pex3/Netflip.java
+++ nhnguyen_tnygren_3/src/edu/mines/nhnguyen/tnygren/pex3/Netflip.java
@@ -1,13 +1,32 @@
 package edu.mines.nhnguyen.tnygren.pex3;
 
-public class Netflip {
+import javax.swing.*;
+import java.awt.*;
 
+public class Netflip extends JApplet {
+
+
+	public void init() {
+		// Execute a job on the event-dispatching thread; creating this applet's GUI.
+		try {
+			SwingUtilities.invokeAndWait(new Runnable() {
+				public void run() {
+					JLabel lbl = new JLabel("Hello World");
+					add(lbl);
+				}
+			});
+		} catch (Exception e) {
+			System.err.println("createGUI didn't complete successfully");
+		}
+	}
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 	}
+	
 
+	private static final long serialVersionUID = 1L;
 }
